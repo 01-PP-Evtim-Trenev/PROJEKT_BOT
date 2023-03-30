0c52252ff630a977f25bc9ee5f36e28f3db8992a
Peter Weber <peter-aalen@gmx.net>
2023-03-30 21:54:11 +0200

    False code

--- bot.js
+++ bot.js
@@ -5,3 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+  const inputField = document.getElementById("input")
+    inputField.addEventListener("keydown", function(e) {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            output(input);
+    }
+  });
+  function output()
+{
+      let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+        …     
+     
+     text = text
+    .replace(/ a /g, " ")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "");
+}
